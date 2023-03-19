6f66004acf324c2a2d695c3fa85f67e0de866fec
Mikita Zubtsou <nzubtsov194@gmail.com>
2023-03-19 11:52:38 +0100

    completed 1 and 2 tasks

---
+++ FirstAndSecondTasks.php
@@ -0,0 +1,40 @@
+<?php
+//First task
+/**
+ * @param int $number
+ * @return string
+ */
+function hashOrder(int $number):string
+{
+    $numberAsString = strval($number);
+    $paddedNumberAsString = str_pad($numberAsString, 7, '0', STR_PAD_LEFT);
+    $reversedNumberAsString = strrev($paddedNumberAsString);
+
+    $result = '';
+    for ($i = 0; $i < 7; $i++) {
+        $digit = intval($reversedNumberAsString[$i]);
+        $resultDigit = 9 - $digit;
+        $result .= $resultDigit;
+    }
+
+    return $result;
+}
+
+//Second Task
+/**
+ * @param string $s
+ * @return int
+ */
+function findUniqueString(string $s): int {
+
+    $splittedString = str_split(strtolower($s));
+    $countValues = array_count_values($splittedString);
+
+    foreach($splittedString as $key => $value) {
+        if($countValues[$value] == 1) {
+            return $key + 1;
+        }
+    }
+    return -1;
+}
+
